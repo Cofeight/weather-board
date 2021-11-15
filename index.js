297a27c93e884c348363d695f632c33b8fe6f7b2
Chris <chris.feight@gmail.com>
2021-11-14 21:01:47 -0800

    testing UV function

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var apiKey = "bf93e30fb6366640ed7b651b24b47a40";
 var cityInput = document.querySelector("#city-input");
 var searchButton = document.querySelector("#search-button");
 const historyEl = document.getElementById("history");
+//var lat = city.coord.lat;
+//var lon = city.coord.lon;
 //moment wasn't working properly without redefining as m
 const m = moment();
 const m1 = moment().add(1, "day");
@@ -11,6 +13,8 @@ const m4 = moment().add(4, "day");
 const m5 = moment().add(5, "day");
 
 searchButton.addEventListener("click", findCity)
+
+
 
 //logs city input
 //clears innerHTML in all infoBoxes
@@ -33,11 +37,10 @@ function findCity() {
         var cityWind = document.createElement("h3")
         var cityHumidity = document.createElement("h3")
 
-        //cityIcon.innerText = data.list[0].weather[0].icon
-        cityName.innerText = data.city.name + m.format(" (MM/DD/YY)")
+        cityName.innerText = data.city.name + m.format(" (MM/DD/YY)") + data.list[0].weather[0].icon //THIS IS THE WEATHER ICON
         cityTemp.innerText = data.list[0].main.temp + " °F"
         cityWind.innerText = data.list[0].wind.speed + " MPH"
-        cityHumidity.innerText = data.list[0].main.humidity + " %"
+        cityHumidity.innerText = data.list[0].main.humidity + "%"
 
         document.querySelector("#infoBox").appendChild(cityIcon)
         document.querySelector("#infoBox").appendChild(cityName)
@@ -55,7 +58,7 @@ function findCity() {
         //cityIcon1.innerText = data.list[7].weather[7].icon
         cityTemp1.innerText = data.list[7].main.temp + " °F"
         cityWind1.innerText = data.list[7].wind.speed + " MPH"
-        cityHumidity1.innerText = data.list[7].main.humidity + " %"
+        cityHumidity1.innerText = data.list[7].main.humidity + "%"
 
         document.querySelector("#infoBox1").appendChild(cityName1)
         //document.querySelector("#infoBox1").appendChild(cityIcon1)
@@ -71,7 +74,7 @@ function findCity() {
         cityName2.innerText = m2.format(" (MM/DD/YY)")
         cityTemp2.innerText = data.list[15].main.temp + " °F"
         cityWind2.innerText = data.list[15].wind.speed + " MPH"
-        cityHumidity2.innerText = data.list[15].main.humidity + " %"
+        cityHumidity2.innerText = data.list[15].main.humidity + "%"
 
         document.querySelector("#infoBox2").appendChild(cityName2)
         document.querySelector("#infoBox2").appendChild(cityTemp2)
@@ -86,7 +89,7 @@ function findCity() {
         cityName3.innerText = m3.format(" (MM/DD/YY)")
         cityTemp3.innerText = data.list[23].main.temp + " °F"
         cityWind3.innerText = data.list[23].wind.speed + " MPH"
-        cityHumidity3.innerText = data.list[23].main.humidity + " %"
+        cityHumidity3.innerText = data.list[23].main.humidity + "%"
 
         document.querySelector("#infoBox3").appendChild(cityName3)
         document.querySelector("#infoBox3").appendChild(cityTemp3)
@@ -101,7 +104,7 @@ function findCity() {
         cityName4.innerText = m4.format(" (MM/DD/YY)")
         cityTemp4.innerText = data.list[31].main.temp + " °F"
         cityWind4.innerText = data.list[31].wind.speed + " MPH"
-        cityHumidity4.innerText = data.list[31].main.humidity + " %"
+        cityHumidity4.innerText = data.list[31].main.humidity + "%"
 
         document.querySelector("#infoBox4").appendChild(cityName4)
         document.querySelector("#infoBox4").appendChild(cityTemp4)
@@ -121,6 +124,18 @@ function findCity() {
         document.querySelector("#infoBox5").appendChild(cityName5)
         document.querySelector("#infoBox5").appendChild(cityTemp5)
         document.querySelector("#infoBox5").appendChild(cityWind5)
-        document.querySelector("#infoBox5").appendChild(cityHumidity5)
+        document.querySelector("#infoBox5").appendChild(cityHumidity5)       
     })
 }
+
+
+//function cityUV () {
+//    console.log("testing UV");
+//    var lat = response.city.coord.lat;
+//    var lon = response.city.coord.lon;
+//    fetch("api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon +"&appid=" + apiKey)
+//    .then(response => response.json())
+//    .then(data => {
+//        console.log(cityUV);
+//    })
+//}
